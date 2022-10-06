a672e56d5f025959ecb22650f733b5421549a189
EllieT7 <elizabeth.tacachira@gmail.com>
2022-10-06 12:17:56 -0400

    Componente form

---
+++ src/components/form.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h5>Ingresa tu nombre:</h5>
+    <input id="nombre" type="text" />
+    <button v-on:click="mandarNombre">Enviar</button>
+    <h5 id="salida"></h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form',
+  methods: {
+    mandarNombre: function (event) {
+      var nombre = document.getElementById('nombre').value;
+      document.getElementById('salida').innerText =
+        'Bienvenido, ' + nombre + ' !';
+    },
+  },
+};
+</script>
+
+<style>
+input {
+  border-radius: 5px 1px solid #979797;
+}
+button {
+  margin: 5px;
+  background-color: #42b983;
+  border: 1px solid white;
+  padding: 5px;
+  color: white;
+}
+* {
+  font-weight: 100;
+}
+</style>
